fe4ac0d62160a6b26a07da161d7cf4d90f055680
kevinquillen <kevin@inclind.com>
2012-11-24 11:50:59 -0500

    Make the CSS a bit more flexible in regards to using list items. Add a new permission to administering the settings. Add a user_access check with the enabled check before displaying the bar. Replace placeholders in the tpl file with variables passed from the preprocess function.

--- support_bar.tpl.php
+++ support_bar.tpl.php
@@ -1,7 +1,7 @@
 <div id="admin-support-bar">
   <ul>
-    <li class="company-phone"><?php print $support_bar_phone_number; ?></li>
-    <li class="company-contact"><a href="#">Email CompanyName</a></li>
-    <li class="company-logo"><a href="#"><img src="<?php print $support_bar_filepath; ?>" alt="" /></a></li>
+    <li class="company-phone with-border"><?php print $phone_number; ?></li>
+    <li class="company-contact"><?php print $email; ?></li>
+    <li class="company-logo"><?php print $logo; ?></li>
   </ul>
 </div>
